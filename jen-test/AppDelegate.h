709b4432fdf8bd8978da5c45fd8c4fa8f8a67546
bai rui <>
2018-05-28 17:44:45 +0800

    first commit

---
+++ jen-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jen-test
+//
+//  Created by bai rui on 2018/5/26.
+//  Copyright © 2018年 bai rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jen-test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jen-test
+//
+//  Created by bai rui on 2018/5/26.
+//  Copyright © 2018年 bai rui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
